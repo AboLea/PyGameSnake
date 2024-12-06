ecf8720652314ccf2e9f8d37ed3a2168822c413c
AboLea <patrik_awad@hotmail.se>
2024-12-06 20:26:14 +0100

    Changes

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
 import pygame
+import pygame_menu
 import random
+import pickle
+
+pygame.init()
+
+width, height = 800, 600
+screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption("Snake")
+
+clock = pygame.time.Clock()
+
+start = True
+
+def start_menu():
+    pygame.display.set_caption("Main menu")
+    while True:
+        screen.fill("black")
+
+def main():
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
 
 
-##Testing a branch
+        if start:
+            start_menu()
+
+
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+
+if __name__ == "__main__":
+    main()
